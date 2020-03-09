98c3d95b87724611b687ebe517c86b218fd390bb
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-03-09 15:49:13 -0400

    Fixed days of week buttons, added string to create weekly schedule

--- app/src/main/java/com/example/ontime/CreateEvent.java
+++ app/src/main/java/com/example/ontime/CreateEvent.java
@@ -2,13 +2,26 @@ package com.example.ontime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class CreateEvent extends AppCompatActivity {
+    public void cancelCreateEvent(View view) {
+        // navigate back to homepage
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
 
     // move to onclick listener
     public void createEvent(View view) {
@@ -18,6 +31,26 @@ public class CreateEvent extends AppCompatActivity {
         EditText destination = (EditText) findViewById(R.id.createEventDestination);
         EditText alarmSound = (EditText) findViewById(R.id.createEventAlarm);
         EditText vibration = (EditText) findViewById(R.id.createEventVibration);
+
+        // create string from days entered - in order "SMTWTFS". For example, if an event is on Tuesday, Wednesday, and Friday: 0011010
+        LinearLayout daysOfWeek = (LinearLayout) findViewById(R.id.daysOfWeek);
+        char[] eventDays = new char[7];
+        for (int i = 0; i < 7; i++) {
+            ToggleButton day = (ToggleButton) daysOfWeek.getChildAt(i);
+            Log.i("aldskfjlkasd", String.valueOf(day.getId()));
+            if (day.isChecked()) {
+                Log.i("checked", "checked");
+                eventDays[i] = '1';
+            } else {
+                eventDays[i] = '0';
+            }
+        }
+
+        String daysString = new String(eventDays);
+        Log.i("days entered", daysString);
+
+
+
         // get days too -> convert into weeklyschedule string
         // add switch for private/public
         // switch for repeat weekly
@@ -32,23 +65,13 @@ public class CreateEvent extends AppCompatActivity {
         //newEvent.addProperty("eventName", name);
         //newEvent.addProperty("time", startTime);
         //newEvent.addProperty("endDate", );
-        //newEvent.addProperty("weeklySchedule", );
+        //newEvent.addProperty("weeklySchedule", daysString);
         //newEvent.addProperty("ownerId", );
         //newEvent.addProperty("startLocation", startLocation);
         //newEvent.addProperty("destination", destination);
         //newEvent.addProperty("startDate", );
         //newEvent.addProperty("repeatWeekly", );
 
-
-        /*
-                lat,
-                lng,
-         */
-
-
-
-
-        // Send json to backend to put into db
 
     }
 
@@ -57,7 +80,9 @@ public class CreateEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+
     }
 
-
 }
+
