86077e8f12b3d6f6b273a7f11d9cad78526621eb
Kevin <Kevinxz01@gmail.com>
2020-03-15 13:30:40 -0400

    Created the side navigation menu

--- app/src/main/java/com/example/ontime/HomePage.java
+++ app/src/main/java/com/example/ontime/HomePage.java
@@ -1,17 +1,26 @@
 package com.example.ontime;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity
-{
+import com.google.android.material.navigation.NavigationView;
+
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Button add_event;
     TextView user_name;
+
+    DrawerLayout drawer_layout;
+    NavigationView navigation_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +30,17 @@ public class HomePage extends AppCompatActivity
 
         add_event = findViewById(R.id.add_event_btn);
         user_name = findViewById(R.id.name_of_user);
+
+        // Menu variables.
+        drawer_layout = findViewById(R.id.drawer_layout);
+        navigation_view = findViewById(R.id.nav_view);
+
+        navigation_view.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer_layout, R.string.navigation_open, R.string.navigation_close);
+        drawer_layout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigation_view.setNavigationItemSelectedListener(this);
 
         add_event.setOnClickListener(new View.OnClickListener()
         {
@@ -34,5 +54,35 @@ public class HomePage extends AppCompatActivity
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case (R.id.invites):
+                Intent intent = new Intent(HomePage.this, Invites.class);
+                startActivity(intent);
+
+            case (R.id.notifications):
+                break;
+
+            case (R.id.private_event):
+                break;
+
+            case (R.id.public_event):
+                break;
+
+            case (R.id.change_password):
+                break;
+
+            case (R.id.log_out_homepage):
+                break;
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
